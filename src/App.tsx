fe003ac5b2025627a7d0c118d9458ad47707a56f
pvt12 <pvt12@gcloud.ua.es>
2025-02-27 17:14:52 +0100

    agregar login

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoginForm from "./components/loginForm";
+
+function App() {
+  return (
+    <>
+      <LoginForm onLogin={() => {}}></LoginForm>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/loginForm.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "../styles/login.css";
+
+// Definimos una interfaz para los props
+interface LoginFormProps {
+  onLogin: (username: string, password: string) => void;
+}
+
+const LoginForm: React.FC<LoginFormProps> = ({ onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onLogin(username, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Nombre de usuario:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Contraseña:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
