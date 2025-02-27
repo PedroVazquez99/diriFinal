7040f49185aa3729916ed1a42ad899f053134190
pvt12 <pvt12@gcloud.ua.es>
2025-02-27 17:31:34 +0100

    Front - Formulario iniciar sesion

--- src/components/loginForm.tsx
+++ src/components/loginForm.tsx
@@ -16,29 +16,64 @@ const LoginForm: React.FC<LoginFormProps> = ({ onLogin }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>
-          Nombre de usuario:
+    <div className="flex items-center justify-center min-h-screen bg-blue-200 rounded-xl">
+      <form
+        className="bg-white shadow-lg rounded-lg px-10 py-12 w-full max-w-md"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-3xl font-extrabold mb-8 text-center text-gray-800">
+          Login
+        </h2>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Nombre de usuario
+          </label>
           <input
+            className="shadow appearance-none border rounded-full w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500"
+            id="username"
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            placeholder="Ingresa tu usuario"
+            required
           />
-        </label>
-      </div>
-      <div>
-        <label>
-          Contraseña:
+        </div>
+        <div className="mb-8">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Contraseña
+          </label>
           <input
+            className="shadow appearance-none border rounded-full w-full py-3 px-4 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500"
+            id="password"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            placeholder="Ingresa tu contraseña"
+            required
           />
-        </label>
-      </div>
-      <button type="submit">Login</button>
-    </form>
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue-500 text-white font-bold py-3 px-6 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 m-1 w-100"
+            type="submit"
+          >
+            Iniciar
+          </button>
+          <button
+            className="bg-blue-500 text-white font-bold py-3 px-6 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 w-100"
+            type="submit"
+          >
+            Registrar
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
