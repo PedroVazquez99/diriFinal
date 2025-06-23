da09179c7f6e676733b6dcd8bd1502a0b6dfb062
pvt12 <pvt12@gcloud.ua.es>
2025-06-23 18:17:21 +0200

    traducciones y composicion de providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { buildProvidersTree } from "./services/providers.tsx";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 
+import { LanguageProvider } from "./providers/LanguageProvider";
+
+const AppProvider = buildProvidersTree([
+  [LanguageProvider],
+  [BrowserRouter],
+  [AuthProvider],
+  // [Provider, { store }],
+  //[ConfigProvider, { theme: globalStyles }],
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </StrictMode>
 );
