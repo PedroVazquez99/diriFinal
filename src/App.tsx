751030fc4a5a9dba03877e961362f98292ef2ef5
pvt12 <pvt12@gcloud.ua.es>
2025-06-29 18:55:31 +0200

    Inicio pagina usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import NavBar from "./components/shared/NavBar";
 import Footer from "./components/shared/Footer";
 import { RegisterRoute } from "./routes/RegisterRoute";
 import InfoMoviePage from "./pages/InfoMoviePage";
+import UsersPage from "./pages/UsersPage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<RegisterRoute><Home /></RegisterRoute>} />
         <Route path="/movie/:id" element={<RegisterRoute><InfoMoviePage /></RegisterRoute>} />
+        <Route path="/users" element={<RegisterRoute><UsersPage /></RegisterRoute>} />
         <Route path="*" element={<Login />} />
       </Routes>
 
